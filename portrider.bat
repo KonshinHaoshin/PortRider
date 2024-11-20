bffebb8a2479711f71f34a5effc32c06393173b7
Dongshan <KonshiHaoshin@gmail.com>
2024-11-20 19:13:50 +0800

    1.0

--- portrider.bat
+++ portrider.bat
@@ -1,12 +1,13 @@
 @echo off
+chcp 65001
 
-:: 检查某个必要的依赖包是否安装（以 requests 为例）
-python -c "import requests" 2>nul
-if %error level% neq 0 (
+:: 检查是否安装了sshtunnel
+python -c "import sshtunnel" 2>nul
+if %errorlevel% neq 0 (
     echo 缺少依赖，正在安装...
     echo 默认通过清华源安装...
     pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-    if %error level% neq 0 (
+    if %errorlevel% neq 0 (
         echo 依赖安装失败，请检查网络或requirements.txt文件。
         pause
         exit /b 1
